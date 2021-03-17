989d96340c43a0e414fcb174529e236e182bee78
XXF <xuanyouwu@163.com>
2021-03-17 14:29:57 +0800

    fix:emojiteditText setText不替换表情

--- lib_emotion/src/main/java/com/xxf/keyboard/emotion/component/EmotionEditText.java
+++ lib_emotion/src/main/java/com/xxf/keyboard/emotion/component/EmotionEditText.java
@@ -1,7 +1,9 @@
 package com.xxf.keyboard.emotion.component;
 
 import android.content.Context;
+import android.text.Editable;
 import android.text.Spannable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 
 import com.xxf.keyboard.emotion.EmojiSpanBuilder;
@@ -15,15 +17,56 @@ public class EmotionEditText extends androidx.appcompat.widget.AppCompatEditText
 
     public EmotionEditText(Context context) {
         super(context);
+        init();
     }
+
 
     public EmotionEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public EmotionEditText(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
+
+    private final TextWatcher emojiTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            try {
+                EmotionEditText.this.removeTextChangedListener(emojiTextWatcher);
+                int selectionEnd = -1;
+                if (!hasSelection()) {
+                    selectionEnd = getSelectionEnd();
+                }
+                setText(s, BufferType.EDITABLE);
+                if (selectionEnd >= 0) {
+                    setSelection(selectionEnd);
+                }
+            } catch (Throwable e) {
+                e.printStackTrace();
+            } finally {
+                init();
+            }
+        }
+    };
+
+    private void init() {
+        this.removeTextChangedListener(emojiTextWatcher);
+        this.addTextChangedListener(emojiTextWatcher);
+    }
+
 
     @Override
     public void setText(CharSequence text, BufferType type) {
